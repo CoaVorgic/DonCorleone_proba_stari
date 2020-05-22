737ac24ce6f40572c395584dba3e722cc19e2053
CoaVorgic <axa.v98@hotmail.rs>
2020-05-22 20:28:11 +0200

    asd

--- dostava.php
+++ dostava.php
@@ -44,15 +44,15 @@
     </div>
     <div style="padding-bottom: 18px;font-size : 24px;" >Dostava</div>
     <div style="padding-bottom: 18px;font-size : 18px;">Ovde mozete online poruciti iz naseg restorana! Sve dostave preko 500 dinara su besplatne!</div>
-    <div style="padding-bottom: 18px;" id="data_2">Jelo<span style="color: #EE0000;"> *</span><br/>
-        <input type="checkbox" name="cezar">
+    <div style="padding-bottom: 18px;">Jelo<span style="color: #EE0000;"> *</span><br/>
+        <input type="checkbox" name="jelo" id="cezar">
         <label for="hrana">Cezar salata - 450 din</label><br>
-        <input type="checkbox" name="biftek">
+        <input type="checkbox" name="jelo" id="biftek">
         <label for="hrana">Biftek - 700 din</label><br>
-        <input type="checkbox" name="lignje">
+        <input type="checkbox" name="jelo" id="lignje">
         <label for="hrana">Lignje na raznju - 1200 din</label><br>
-        <span>Quantity<span style="color: #EE0000;"> *</span></span>
-        <select id="option_1" name="opcije1" style="width : 250px;" class="form-control">
+        <span>Kolicina<span style="color: #EE0000;"> *</span></span>
+        <select id="option_1" name="kolicina" style="width : 250px;" class="form-control">
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -65,15 +65,15 @@
             <option>10</option>
         </select>
     </div>
-    <div style="padding-bottom: 18px;" id="data_4">Pice<span style="color: #EE0000;"> *</span><br/>
-        <input type="checkbox" id="kola" name="kola">
+    <div style="padding-bottom: 18px;">Pice<span style="color: #EE0000;"> *</span><br/>
+        <input type="checkbox" id="kola" name="pice">
         <label for="pice">Koka kola 2l - 110 din</label><br>
-        <input type="checkbox" id="vino" name="vino">
+        <input type="checkbox" id="vino" name="pice">
         <label for="pice">Vino Zdrepceva krv - 250 din</label><br>
-        <input type="checkbox" id="knjaz" name="knjaz">
+        <input type="checkbox" id="knjaz" name="pice">
         <label for="pice">Knjaz Milos 1,5l - 70 din</label><br>
-        <span>Quantity<span style="color: #EE0000;"> *</span></span>
-        <select id="option_2" name="opcije2" style="width : 250px;" class="form-control">
+        <span>Kolicina<span style="color: #EE0000;"> *</span></span>
+        <select id="option_2" name="kolicina1" style="width : 250px;" class="form-control">
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -87,7 +87,7 @@
         </select>
     </div>
     <div style="padding-bottom: 18px;">Phone<span style="color: #EE0000"> *</span><br/>
-        <input type="text" id="data_6" name="phone" style="width : 250px;" class="form-control"/>
+        <input type="text" id="data_6" name="telefon" style="width : 250px;" class="form-control"/>
     </div>
     <div style="padding-bottom: 18px;">Adresa<span style="color: #EE0000"> *</span><br/>
         <input type="text" id="data_8" name="adresa" style="width : 250px;" class="form-control"/>
@@ -101,17 +101,17 @@
     <div style="padding-bottom: 18px;">Komentar / Dodatni zahtev<br/>
         <textarea id="data_9" name="comment" style="width : 400px;" rows="4" class="form-control"></textarea>
     </div>
-    <div style="padding-bottom: 18px;"><input name="submit" value="Submit" type="submit"/></div>
+    <div style="padding-bottom: 18px;"><input name="submit" value="Posalji" type="submit"/></div>
 </form>
 
 
 <script type="text/javascript">
     function validateForm() {
-        if (isEmpty(document.getElementById('data_2').value.trim())) {
+        if (isEmpty(document.getElementById('cezar').value.trim())) {
             alert('One checkbox is required!');
             return false;
         }
-        if (isEmpty(document.getElementById('data_4').value.trim())) {
+        if (isEmpty(document.getElementById('kola').value.trim())) {
             alert('One checkbox is required!');
             return false;
         }
